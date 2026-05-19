4e68093f08cbaf17686b5bc376b3d762f72ea56f
adriano020 <adriano020@gmail.com>
2026-05-19 20:14:17 -0300

    Add files via upload

--- index.py
+++ index.py
@@ -3,15 +3,14 @@ import streamlit as st
 st.title("Lista de Tarefas")
 
 #Criar lista na memoria
-
 if "tarefas" not in st.session_state:
     st.session_state.tarefas = []
 
 #Campo de texto
-nova_tarefa = st.text_input("Digite uma tarefa")
+nova_tarefa = st.text_input("Digite uma Tarefa")
 
 #Botao Adicionar
-if st.buton("Adicionar"):
+if st.button("Adicionar"):
 
     if nova_tarefa != "":
 
@@ -20,65 +19,22 @@ if st.buton("Adicionar"):
             "concluida": False
         }
         st.session_state.tarefas.append(tarefa)
-
-#verificação de campo vazio
-if st.button("adicionar"):
-
-    if nova_tarefa != "":
-
-        tarefa = {
-            "nome": nova_tarefa,
-            "concluida": False
-        }
-        st.session_state.tarefas.append(tarefa)
-
-#Criando Dicionario
-
-if st.button("adicionar"):
-
-    if nova_tarefa != "":
-
-        tarefa = {
-            "nome": nova_tarefa,
-            "concluida": False
-        }
-        st.session_state.tarefas.append(tarefa)
-
-#Adicionar na lista
-
-if st.button("adicionar"):
-
-    if nova_tarefa != "":
-
-        tarefa = {
-            "nome": nova_tarefa,
-            "concluida": False
-        }
-        st.session_state.tarefas.append(tarefa)
-
-#Subtitulo
-
 st.subheader("Minhas Tarefas")
 
 for i, tarefa in enumerate(st.session_state.tarefas):
 
-#Estrutura de reptição
-
-st.subheader("Minhas Tarefas")
-
-for i, tarefa in enumerate(st.session_state.tarefas):
     col1, col2 = st.columns([5, 1])
 
     with col1:
         concluida = st.checkbox(
             tarefa["nome"],
-            value = tarefa["concluida"]
-            key = i
-        )
+            value=tarefa["concluida"],
+            key=i
+            )
 
         tarefa["concluida"] = concluida
     
     with col2:
-        if st.button("x", key=f["delete{i}"]):
-            st.session_state.tarefas.pop("i")
+        if st.button("x", key=f"delete{i}"):
+            st.session_state.tarefas.pop(i)
             st.rerun()
